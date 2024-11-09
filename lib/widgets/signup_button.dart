b6e414d0f8141ce7930e12222d961a94dc058428
Prathamesh Patil <prathameshp@thoughtsketch.com>
2024-11-09 12:42:48 +0530

    added_signup_button

---
+++ lib/widgets/signup_button.dart
@@ -0,0 +1,42 @@
+//button
+
+import 'package:flutter/material.dart';
+
+class MButton extends StatelessWidget {
+  final String text;
+
+  const MButton({required this.text, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const primaryColor = Color(0xff4338CA);
+    const secondaryColor = Color(0xff6D28D9);
+    const accentColor = Color(0xffffffff);
+   
+    const double borderRadius = 15;
+
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(borderRadius),
+        gradient: const LinearGradient(colors: [primaryColor, secondaryColor]),
+      ),
+      child: ElevatedButton(
+        style: ButtonStyle(
+          elevation: MaterialStateProperty.all(0),
+          alignment: Alignment.center,
+          padding: MaterialStateProperty.all(const EdgeInsets.only(
+            right: 75, left: 75, top: 15, bottom: 15)),
+          backgroundColor: MaterialStateProperty.all(Colors.transparent),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(borderRadius)),
+          ),
+        ),
+        onPressed: null, // Button is disabled with null onPressed
+        child: Text(
+          text,
+          style: const TextStyle(color: accentColor, fontSize: 16),
+        ),
+      ),
+    );
+  }
+}
